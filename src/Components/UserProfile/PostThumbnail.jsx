e623833f6c72ce3a8389867ef67bd08452c3a146
sibichandru-ubuntu <sibichandru-ubuntu@U-3793G8BJLI3OK.element5.ai>
2024-10-18 13:00:38 +0530

    completed user profile Need to add Redux and backend Integration

---
+++ src/Components/UserProfile/PostThumbnail.jsx
@@ -0,0 +1,8 @@
+import './PostThumbnail.scss';
+export const PostThumbnail = (props) => {
+  return (
+    <div className='UserProfile-PostThumbnail'>
+      <img src={props.thumbnail} alt='' height='350px' width='350px' />
+    </div>
+  );
+};
---
+++ src/Pages/UserProfile/Index.jsx
@@ -0,0 +1,9 @@
+import { Userinfo } from '../../Components/UserProfile/Userinfo.jsx';
+
+export const UserProfile = () => {
+  return (
+    <div>
+      <Userinfo />
+    </div>
+  );
+};
